d97d6783253d95daf0bcc5337e5630bc0b0c6698
Nahuel <nahueltecnicomdp@gmail.com>
2025-11-09 14:53:28 -0300

    a

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -25,7 +25,7 @@ const variants = {
 
 export default function Hero() {
   const phrases = [
-    "Palabra",
+    "A una fraccion del costo",
     "Idea brillante",
     "Proyecto nuevo",
     "Sueño grande",
@@ -41,20 +41,38 @@ export default function Hero() {
   }, []);
 
   return (
-    <section className="flex items-center justify-center px-11 py-8">
-      <section className="w-full min-h-[90vh] bg-[#a282ef] text-white rounded-[64px] relative lg:px-36 flex items-center justify-between">
+    <section className="flex flex-col md:flex-row items-center justify-center px-4 md:px-11 py-8 relative bg-[#e8d8ff]">
+      <section className="bg-white absolute rounded-b-4xl top-0 left-0 z-10">
+        <Image
+          src="/Logos/icono quippos.svg"
+          alt="Logo"
+          className="hidden md:flex"
+          width={100}
+          height={100}
+        />
+      </section>
+      <section className="w-full min-h-[90vh] bg-[#a780f5] text-white rounded-3xl md:rounded-[64px] relative px-6 md:py-0 md:px-36 flex items-center justify-between flex-col md:flex-row">
         <div className="copy">
-          <Image src="/logo.png" alt="Logo" width={100} height={100} />
-          <h2 className="text-7xl font-semibold">
-            Gestiona tu talento <br /> como una gran <br /> corporación
+          <Image
+            src="/logo.png"
+            alt="Logo"
+            className="hidden md:flex"
+            width={100}
+            height={100}
+          />
+          <h2 className="text-4xl lg:text-7xl font-semibold">
+            <span className="text-[#27067f]">
+              Gestioná tu <i>talento</i>
+            </span>{" "}
+            <br /> como una gran <br /> corporación
           </h2>
 
-          <div className="p-2 flex items-center w-full max-w-sm bg-purple-800 rounded-full my-11">
-            <span className="bg-purple-200 rounded-full h-12 w-12 flex items-center justify-center">
-              <Plus className="w-8 h-8 text-purple-800" />
+          <div className="p-2 flex items-center w-full  md:max-w-sm bg-[#9a69f9] rounded-full my-6 md:my-11">
+            <span className="bg-[#27067f] rounded-full h-8 w-8 flex items-center justify-center">
+              <Plus className="w-6 h-6 text-[#f0f0f0]" />
             </span>
 
-            <div className="ml-3 flex-1 overflow-hidden">
+            <div className="ml-3 flex-1 overflow-hidden ">
               {/* “Ventana” fija del elevador */}
               <div className="relative h-6 overflow-hidden">
                 <AnimatePresence initial={false} mode="wait">
@@ -65,7 +83,7 @@ export default function Hero() {
                     animate="center"
                     exit="exit"
                     transition={{ duration: 0.4, ease: "easeInOut" }}
-                    className="absolute inset-0 flex items-center text-sm font-medium text-purple-50 leading-6"
+                    className="absolute inset-0 flex items-center text-xl font-medium text-purple-50 leading-6"
                   >
                     {phrases[index]}
                   </motion.p>
@@ -74,44 +92,44 @@ export default function Hero() {
             </div>
           </div>
 
-          <p className="text-2xl ">
+          <p className="text-md whitespace-nowrap">
             Impulsa el crecimiento de tu equipo con <br /> una plataforma HCM
             que integra la gestión <br /> de procesos y mejora la comunicación.
           </p>
         </div>
 
-        <div className="formulario">
-          <Card className="relative py-11 md:w-md px-8">
-            <CardTitle className="bg-purple-200 absolute top-0 left-0 font-medium text-2xl">
+        <div className="formulario mt-6 pb-10">
+          <Card className="relative py-11 md:w-md px-6 outline-none border-none pb-4">
+            <CardTitle className="bg-[#27067f] absolute top-0 left-0 font-medium text-xl px-4 rounded-br-3xl py-2 md:text-2xl text-white">
               Solicitar demo gratuita
             </CardTitle>
 
-            <div className="space-y-6">
+            <div className="space-y-4 mt-4">
               <div className="space-y-2">
                 <Label>Nombre y Apellido</Label>
 
-                <Input className="bg-gray-200 rounded-full py-4 px-2" />
+                <Input className="bg-gray-200 rounded-full py-2 px-2" />
               </div>
               <div className="space-y-2">
                 <Label>Email laboral</Label>
 
-                <Input className="bg-gray-200 rounded-full py-4 px-2" />
+                <Input className="bg-gray-200 rounded-full py-2 px-2" />
               </div>
               <div className="space-y-2">
                 <Label>Industria</Label>
 
-                <Input className="bg-gray-200 rounded-full py-4 px-2" />
+                <Input className="bg-gray-200 rounded-full py-2 px-2" />
               </div>
               <div className="space-y-2">
                 <Label>Pais</Label>
 
-                <Input className="bg-gray-200 rounded-full py-4 px-2" />
+                <Input className="bg-gray-200 rounded-full py-2 px-2" />
               </div>
               <div className="flex items-center gap-2">
                 <div className="space-y-2">
                   <Label>Cantidad de Empleados</Label>
 
-                  <Input className="bg-gray-200 rounded-full py-4 px-2" />
+                  <Input className="bg-gray-200 rounded-full py-2 px-2" />
                 </div>
                 <Button>
                   Enviar{" "}
@@ -125,7 +143,7 @@ export default function Hero() {
         </div>
       </section>
 
-      <div className="absolute bottom-0 right-20 rounded-t-3xl bg-purple-800">
+      <div className="absolute bottom-0 right-20 rounded-t-3xl bg-purple-800 md:flex hidden">
         <Image src="/logo.png" alt="Logo" width={100} height={100} />
 
         <p>
