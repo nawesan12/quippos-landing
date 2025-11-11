4484e39ab753c23a1764b94131810c8d6fd3327c
Nahuel <nahueltecnicomdp@gmail.com>
2025-11-11 15:43:54 -0300

    a

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -49,7 +49,7 @@ export default function Hero() {
           height={1000}
         />
       </section>
-      <section className="w-full min-h-[90vh] bg-[#a780f5] text-white rounded-4xl md:rounded-[64px] relative px-8 md:py-0 md:px-40 flex items-center justify-between flex-col md:flex-row pt-32 md:pt-0">
+      <section className="w-full min-h-[90vh] bg-[#a780f5] text-white rounded-4xl md:rounded-[64px] relative px-10 md:py-0 md:px-40 flex items-center justify-between flex-col md:flex-row pt-32 md:pt-0">
         <div className="copy w-full">
           <Image
             src="/quippos1.png"
@@ -65,7 +65,7 @@ export default function Hero() {
             <br /> como una gran <br /> corporación
           </h2>
 
-          <div className="p-2 flex items-center w-full md:max-w-md bg-[#9a69f9] rounded-full my-6 md:my-11">
+          <div className="md:p-2 px-1 py-1 flex items-center w-full md:max-w-md bg-[#9a69f9] rounded-full my-6 md:my-11">
             <span className="bg-[#27067f] rounded-full h-9 w-9 flex items-center justify-center">
               <Plus className="w-7 h-7 text-[#f0f0f0]" strokeWidth={3} />
             </span>
@@ -98,48 +98,48 @@ export default function Hero() {
 
         <div className="formulario mt-6 pb-10">
           <Card className="relative py-11 md:w-md px-6 outline-none border-none pb-4 rounded-4xl">
-            <CardTitle className="bg-[#27067f] absolute top-0 left-0 font-medium text-xl px-4 rounded-br-3xl py-2 md:text-2xl text-white ">
+            <CardTitle className="bg-[#27067f] absolute top-0 left-0 font-medium text-sm px-4 rounded-br-full pr-6 md:rounded-br-3xl py-2 md:text-2xl text-white ">
               Solicitar demo gratuita
             </CardTitle>
 
-            <div className="space-y-4 mt-8">
+            <div className="space-y-2 md:space-y-4 mt-1 md:mt-8">
               <div className="space-y-2">
-                <Label className="whitespace-nowrap text-xs md:text-base">
+                <Label className="whitespace-nowrap text-[10px] md:text-base">
                   Nombre y Apellido
                 </Label>
 
-                <Input className="bg-[#f0f0f0] rounded-full py-2 px-2" />
+                <Input className="bg-[#f0f0f0] rounded-full py-1 md:py-2 px-2" />
               </div>
               <div className="space-y-2">
-                <Label className="whitespace-nowrap text-xs md:text-base">
+                <Label className="whitespace-nowrap text-[10px] md:text-base">
                   Email laboral
                 </Label>
 
-                <Input className="bg-[#f0f0f0] rounded-full py-2 px-2" />
+                <Input className="bg-[#f0f0f0] rounded-full py-1 md:py-2 px-2" />
               </div>
               <div className="space-y-2">
-                <Label className="whitespace-nowrap text-xs md:text-base">
+                <Label className="whitespace-nowrap text-[10px] md:text-base">
                   Industria
                 </Label>
 
-                <Input className="bg-[#f0f0f0] rounded-full py-2 px-2" />
+                <Input className="bg-[#f0f0f0] rounded-full py-1 md:py-2 px-2" />
               </div>
               <div className="space-y-2">
-                <Label className="whitespace-nowrap text-xs md:text-base">
+                <Label className="whitespace-nowrap text-[10px] md:text-base">
                   Pais
                 </Label>
 
-                <Input className="bg-[#f0f0f0] rounded-full py-2 px-2" />
+                <Input className="bg-[#f0f0f0] rounded-full py-1 md:py-2 px-2" />
               </div>
               <div className="grid grid-cols-10 gap-2 place-items-center">
                 <div className="space-y-2 col-span-6 w-full">
-                  <Label className="whitespace-nowrap text-xs md:text-base">
+                  <Label className="whitespace-nowrap text-[10px] md:text-base">
                     Cantidad de Empleados
                   </Label>
 
-                  <Input className="bg-[#f0f0f0] rounded-full py-2 px-2" />
+                  <Input className="bg-[#f0f0f0] rounded-full py-1 md:py-2 px-2" />
                 </div>
-                <button className="col-span-4 rounded-full bg-[#e8d8ff] text-[#27067f] md:py-1 py-2 font-semibold px-2 max-h-max w-full flex items-center justify-between text-base md:text-2xl md:pl-6 pl-2 relative top-2 md:top-4">
+                <button className="col-span-4 rounded-full bg-[#e8d8ff] text-[#27067f] md:py-1 py-2 font-semibold px-1 md:px-2 max-h-max w-full flex items-center justify-between text-sm md:text-2xl md:pl-6 pl-2 relative top-2 md:top-4">
                   Enviar{" "}
                   <span className="bg-[#f0f0f0] rounded-full h-6 w-6 grid place-items-center">
                     <ArrowRight color="#27067f" />
