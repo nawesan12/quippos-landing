766ca32a1f16dbcaed8ee1a76ed395d75b22007d
Nahuel <nahueltecnicomdp@gmail.com>
2025-11-12 12:11:28 -0300

    a

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -107,40 +107,40 @@ export default function Hero() {
 
             <div className="space-y-1 md:space-y-4 mt-1 md:mt-8">
               <div className="space-y-1 md:space-y-2">
-                <Label className="whitespace-nowrap text-[10px] md:text-base">
+                <Label className="whitespace-nowrap text-xs md:text-base">
                   Nombre y Apellido
                 </Label>
 
-                <Input className="bg-[#f0f0f0] h-6 md:h-auto md:text-base text-sm rounded-full py-1 md:py-2 px-2" />
+                <Input className="bg-[#f0f0f0] h-8 md:h-auto md:text-base text-sm rounded-full py-1 md:py-2 px-2" />
               </div>
               <div className="space-y-1 md:space-y-2">
-                <Label className="whitespace-nowrap text-[10px] md:text-base">
+                <Label className="whitespace-nowrap text-xs md:text-base">
                   Email laboral
                 </Label>
 
-                <Input className="bg-[#f0f0f0] h-6 md:h-auto md:text-base text-sm rounded-full py-1 md:py-2 px-2" />
+                <Input className="bg-[#f0f0f0] h-8 md:h-auto md:text-base text-sm rounded-full py-1 md:py-2 px-2" />
               </div>
               <div className="space-y-1 md:space-y-2">
-                <Label className="whitespace-nowrap text-[10px] md:text-base">
+                <Label className="whitespace-nowrap text-xs md:text-base">
                   Industria
                 </Label>
 
-                <Input className="bg-[#f0f0f0] h-6 md:h-auto md:text-base text-sm rounded-full py-1 md:py-2 px-2" />
+                <Input className="bg-[#f0f0f0] h-8 md:h-auto md:text-base text-sm rounded-full py-1 md:py-2 px-2" />
               </div>
               <div className="space-y-1 md:space-y-2">
-                <Label className="whitespace-nowrap text-[10px] md:text-base">
+                <Label className="whitespace-nowrap text-xs md:text-base">
                   País
                 </Label>
 
-                <Input className="bg-[#f0f0f0] h-6 md:h-auto md:text-base text-sm rounded-full py-1 md:py-2 px-2" />
+                <Input className="bg-[#f0f0f0] h-8 md:h-auto md:text-base text-sm rounded-full py-1 md:py-2 px-2" />
               </div>
               <div className="grid grid-cols-10 gap-2 place-items-center">
                 <div className="space-y-1 md:space-y-2 col-span-6 w-full">
-                  <Label className="whitespace-nowrap text-[10px] md:text-base">
+                  <Label className="whitespace-nowrap text-xs md:text-base">
                     Cantidad de Empleados
                   </Label>
 
-                  <Input className="bg-[#f0f0f0] h-6 md:h-auto md:text-base text-sm rounded-full py-1 md:py-2 px-2" />
+                  <Input className="bg-[#f0f0f0] h-8 md:h-auto md:text-base text-sm rounded-full py-1 md:py-2 px-2" />
                 </div>
                 <button className="col-span-4 rounded-full cursor-pointer bg-[#e8d8ff] text-[#27067f] md:py-1 py-1 font-semibold px-1 md:px-2 max-h-max w-full flex items-center justify-between text-xs md:text-2xl md:pl-6 pl-2 relative top-2 md:top-4">
                   Enviar{" "}
