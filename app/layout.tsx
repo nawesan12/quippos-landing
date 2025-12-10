6083b428f9952aacf825e48d96cd0c3fd8866deb
Nahuel Santillan <84944031+nawesan12@users.noreply.github.com>
2025-12-10 02:21:34 -0300

    Fix noscript tag with TypeScript error comment

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,8 +40,8 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
       </head>
       <body className={`${poppins.variable} font-sans antialiased`}>
         {children}
-                <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5BXL2LLR"
-        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        {/*@ts-expect-error bla*/}
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5BXL2LLR" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
       </body>
     </html>
   );
